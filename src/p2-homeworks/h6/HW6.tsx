5c0b0af3b11d266213d0bd002d6f3f18a4a7ef59
UserPasha <pasharoma13@gmail.com>
2022-04-05 22:10:40 +0300

    HW 6 is done

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import c from './HW6.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,20 +11,21 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState<string>('editable-span-value', value))
+
     }
 
     return (
         <div>
             <hr/>
-            homeworks 6
 
+            <div className={c.wrapper}>
             {/*should work (должно работать)*/}
             <div>
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
+                    spanProps={{children: value ? undefined : ' enter text...'}}
                 />
             </div>
             <SuperButton onClick={save}>save</SuperButton>
@@ -32,6 +34,8 @@ function HW6() {
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperEditableSpan/>*/}
+
+            </div>
             <hr/>
         </div>
     )
--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,5 +1,6 @@
 import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from 'react'
 import SuperInputText from '../../../h4/common/c1-SuperInputText/SuperInputText'
+import c from './SuperEditableSpan.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -34,19 +35,23 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         // setEditMode() // выключить editMode при нажатии Enter
 
         onEnter && onEnter()
+        setEditMode(false)
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // setEditMode() // выключить editMode при нажатии за пределами инпута
 
         onBlur && onBlur(e)
+        setEditMode(false)
     }
     const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
         // setEditMode() // включить editMode при двойном клике
 
         onDoubleClick && onDoubleClick(e)
+        setEditMode(true)
     }
 
-    const spanClassName = `${'сделать красивый стиль для спана'} ${className}`
+    // const spanClassName = `${'сделать красивый стиль для спана'} ${className}`
+    const spanClassName = c.editWrapper
 
     return (
         <>
@@ -67,6 +72,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
+                        &#9998;
                         {children || restProps.value}
                     </span>
                 )
